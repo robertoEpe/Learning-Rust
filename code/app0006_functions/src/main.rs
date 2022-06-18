a4961ee533f5cdcf2f0fa3189fb9b06b47b04435
Roberto Prince <roberto_epe@hotmail.com>
2022-06-18 06:51:52 -0500

    Project to demo functions and parse

---
+++ code/app0006_functions/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Enter a number: ");
+    
+    // Read the line using our function and parse to float
+    let number:f64 = read_line().parse().unwrap_or(0.0); //  unwrap_or let us use a defualt value if it fails
+    println!("The double plus one is: {}", number * 2.0 + 1.0);
+}
+
+// declare a function that returs a String. This is cheap because it moves by the value.
+fn read_line() -> String {
+    let mut result = String::new();
+    std::io::stdin().read_line(&mut result).expect("Failed to read a line from stdin");
+    result
+}
