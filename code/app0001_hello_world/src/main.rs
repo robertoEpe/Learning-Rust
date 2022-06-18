75624b77ad3887d7d567cc2b2b80551e41661c6f
Roberto Prince <roberto_epe@hotmail.com>
2022-06-17 21:46:02 -0500

    Added first proyect "hello,world"

---
+++ code/app0001_hello_world/src/main.rs
@@ -0,0 +1,7 @@
+// This is comment, they are ignored by the computer
+
+// All programs start at main
+fn main() {
+    // a macro that prints stuff to the standard output 
+    println!("Hello, world, how are you?!");
+}
