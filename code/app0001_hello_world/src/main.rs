20f1b0b0e5e9ade2dd831d5d3b0f9cdf837c5c40
Roberto Prince <roberto_epe@hotmail.com>
2022-06-17 21:46:59 -0500

    Comment explaining strings added

--- code/app0001_hello_world/src/main.rs
+++ code/app0001_hello_world/src/main.rs
@@ -1,7 +1,12 @@
-// This is comment, they are ignored by the computer
+// This is comment, comments are ignored by the computer
 
 // All programs start at main
 fn main() {
     // a macro that prints stuff to the standard output 
     println!("Hello, world, how are you?!");
+
+    /* Other style of comments
+
+        Everything inside "text" are called strings. They are text
+    */
 }
